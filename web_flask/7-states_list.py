124cccb75888d95eb24aca94da6d9c1395e8ea3b
Jassocc <clnearle@gmail.com>
2024-01-26 15:27:12 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_storage(exception):
-    """
-    remove current sqlalchemy session
-    """
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
@@ -24,6 +16,13 @@ def states_list():
     """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    remove current sqlalchemy session
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
